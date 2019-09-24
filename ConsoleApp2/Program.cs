cbae05a4c56a6843cc1146900a6f89d375bb7063
Tomasz Mozgwa <t.mozgwa@sente.pl>
2019-09-24 09:01:30 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"C:\Users\tmozgwa\source\repos\ConsoleApp2\ConsoleApp2\time.txt";
+            List<string> time = new List<string>();
+            int i;
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(file))
+                {
+                    i = 0;
+
+                    do
+                    {
+                        time.Add(sr.ReadLine());
+                        i++;
+                    }
+                    while ((time[i-1] != null));
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"Nie mozna odczytać pliku: {file}");
+                Console.WriteLine(e.Message);
+            }
+
+            //SUMA
+            
+            TimeSpan timeSpanSuma = new TimeSpan();
+            int hoursFromDays = 0;
+            int hours = 0;
+            int minutes = 0;
+
+            foreach (var item in time)
+            {
+                if (item != null)
+                {
+                    DateTime dateTime = DateTime.ParseExact(item, "H:mm", CultureInfo.InvariantCulture);
+                    hours = dateTime.Hour;
+                    minutes = dateTime.Minute;
+                    TimeSpan timeSpan = new TimeSpan(hours, minutes, 0);
+                    timeSpanSuma += timeSpan;
+                }
+            }
+            hoursFromDays = timeSpanSuma.Days * 24 + timeSpanSuma.Hours;
+            Console.WriteLine(hoursFromDays +"h "+ timeSpanSuma.Minutes.ToString()+"min");
+            Console.ReadKey();
+        }
+    }
+}
+
